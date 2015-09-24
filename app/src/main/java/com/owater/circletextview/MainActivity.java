b9de4f917a2787954bd783107f0fdb7ce3eeed5b
Owater <1006340261@qq.com>
2015-09-24 19:42:33 +0800

    CircleTextView Demo

--- app/src/main/java/com/owater/circletextview/MainActivity.java
+++ app/src/main/java/com/owater/circletextview/MainActivity.java
@@ -2,8 +2,6 @@ package com.owater.circletextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
